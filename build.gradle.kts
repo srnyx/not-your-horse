b815d365500f0a90aa6a38a63860725d6996651b
srnyx <25808801+srnyx@users.noreply.github.com>
2024-11-16 21:47:24 -0500

    Update Gradle, plugins, and dependencies
    Gradle: 8.1.1 -> 8.11
    Gradle Galaxy: 1.1.2 -> 1.3.2
    Shadow: 8.1.1 -> 8.3.5
    PlaceholderAPI: 2.11.3 -> 2.11.6

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,11 +6,12 @@ import xyz.srnyx.gradlegalaxy.utility.spigotAPI
 
 plugins {
     java
-    id("xyz.srnyx.gradle-galaxy") version "1.1.2"
-    id("com.github.johnrengelman.shadow") version "8.1.1"
+    id("xyz.srnyx.gradle-galaxy") version "1.3.2"
+    id("com.gradleup.shadow") version "8.3.5"
 }
 
 setupAnnoyingAPI("4.1.0", "xyz.srnyx", "2.0.0", "Prevent players from riding other players' horses, donkeys, and mules")
 spigotAPI("1.8.8")
+
 repository(Repository.PLACEHOLDER_API)
-dependencies.compileOnly("me.clip", "placeholderapi", "2.11.3")
+dependencies.compileOnly("me.clip", "placeholderapi", "2.11.6")
